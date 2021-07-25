d11cb2248a0a7f9aa9e4d9a65aadbf097161710d
Mike <taurenmike@gmail.com>
2021-07-25 13:30:52 -0500

    working on actual html now

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Loaded Javascript!");
+
+var legoCosts = [
+    {"ash": 1250, "cinders": 0},    // rank 1
+    {"ash": 2000, "cinders": 0},    // rank 2
+    {"ash": 3200, "cinders": 0},    // rank 3
+    {"ash": 5150, "cinders": 0},    // rank 4
+    {"ash": 5150, "cinders": 1100}, // rank 5
+    {"ash": 5150, "cinders": 1650}, // rank 6
+]
